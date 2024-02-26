8beb54c7e0a893bb2dd187148757e5e94f6a5420
Abhinav Kutwal <Kutwalabhinav9696@gmail.com>
2024-02-26 11:07:57 +0530

    abbbbcccc

--- javapractiserepo/abc.java
+++ javapractiserepo/abc.java
@@ -1,6 +1,10 @@
 class abc {
     public static void main(String[] args) {
         System.out.println("i am abhinav suresh kutwal");
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
     }
 
 }
